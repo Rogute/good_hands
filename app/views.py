9aa3a02133c706d237cb85c3daa6a7f9941addcc
Rogute <danielostrowski00@gmail.com>
2020-11-13 22:14:00 +0100

    upgrading templates

--- app/views.py
+++ app/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render
 from django.views import View
+from .models import Donation
+from django.db.models import Sum
 
 
 class LandingPage(View):
     def get(self, request):
-        return render(request, "index.html")
+        # quantity from Donation
+        sum_of_bags = Donation.objects.aggregate(Sum('quantity'))
+        ctx = {
+            "sum_of_bags": sum_of_bags
+        }
+        return render(request, "index.html", ctx)
 
 
 class AddDonation(View):
     def get(self, request):
-        return render(request, "form-confirmation.html")
+        return render(request, "form.html")
 
 
 class Login(View):
