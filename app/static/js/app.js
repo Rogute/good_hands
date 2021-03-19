a711c340490cf264bd0f4838b8e8b08f351f9bb4
Rogute <danielostrowski00@gmail.com>
2021-03-19 19:28:54 +0100

    working form from JS

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -245,7 +245,7 @@ document.addEventListener("DOMContentLoaded", function() {
      * TODO: validation, send data to server
      */
     submit(e) {
-      e.preventDefault();
+      // e.preventDefault();
       this.currentStep++;
       this.updateForm();
     }
